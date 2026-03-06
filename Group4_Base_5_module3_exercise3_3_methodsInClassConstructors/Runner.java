e38e4534333409846b0f5e1c361b83563629c222
erusrxmos@gmail.com <erusrxmos@gmail.com>
2026-03-06 16:15:16 +0800

    Runner Class Creation

---
+++ Group4_Base_5_module3_exercise3_3_methodsInClassConstructors/Runner.java
@@ -0,0 +1,16 @@
+package Group4_Base_5_module3_exercise3_3_methodsInClassConstructors;
+
+/*
+Galang, Patrick James
+Garcia, Dolf Philippe
+Lamio, Adinoel Rodge
+Payawal, Matthew James
+Ramos, Erus
+ITE 012-CS12S4
+March 6, 2026
+Topic: Application of Methods in Class Constructors
+*/
+
+public class Runner {
+    
+}
