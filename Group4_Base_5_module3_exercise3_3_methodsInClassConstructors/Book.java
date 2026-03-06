c2ce66fe8a7eb46a3d2a245e5a086958835c292d
erusrxmos@gmail.com <erusrxmos@gmail.com>
2026-03-06 15:35:01 +0800

    Book Class Initialization and overwrite default constructor

--- Group4_Base_5_module3_exercise3_3_methodsInClassConstructors/Book.java
+++ Group4_Base_5_module3_exercise3_3_methodsInClassConstructors/Book.java
@@ -1,5 +1,15 @@
 package Group4_Base_5_module3_exercise3_3_methodsInClassConstructors;
 
 public class Book {
-    
+    int bookId;
+    String title;
+    String ISBN;
+    String category;
+
+    public Book(int bookId, String title, String ISBN, String category) {
+        this.bookId = bookId;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.category = category;
+}
 }
